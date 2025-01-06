5b0e829fc747883b34e8afbe8c4ff19a3fbd37f4
liviakobayashii <liviakmm10@gmail.com>
2025-01-06 15:58:23 -0300

    feat: criação do componente de vizualização de resultados

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,3 +1,31 @@
-export const Results = () => {
-  return <div>...</div>;
+import { Question } from "@/types/Question";
+
+type Props = {
+  questions: Question[];
+  answers: number[];
+};
+
+export const Results = ({ questions, answers }: Props) => {
+  return (
+    <div>
+      {questions.map((item, key) => (
+        <div key={key} className="mb-3">
+          <div className="font-bold">
+            {key + 1}.{item.question}
+          </div>
+
+          <div>
+            <span>
+              (
+              {item.answer === answers[key]
+                ? "Resposta correta"
+                : "Resposta errada"}
+              ) -{" "}
+            </span>
+            {item.options[item.answer]}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
